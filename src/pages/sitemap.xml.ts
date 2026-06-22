b123eea7cbeda6f5ed1bcd4b95e2173110fb1999
norrishui-cmd <norris.hui@gmail.com>
2026-06-22 20:44:36 +0800

    Create sitemap.xml.ts

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,12 @@
+import type { APIRoute } from 'astro';
+import { SITE } from '../consts';
+
+const paths = ['/', '/release-date', '/beginner-guide', '/nokturna', '/romance', '/cheats', '/about', '/privacy'];
+
+export const GET: APIRoute = () => {
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${paths.map((p) => `  <url><loc>${new URL(p, SITE.url).href}</loc></url>`).join('\n')}
+</urlset>`;
+  return new Response(body, { headers: { 'Content-Type': 'application/xml; charset=utf-8' } });
+};
